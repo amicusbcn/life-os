b44227099794baa37af6837815f8d62c03fdff1f
Amicus Bcn <chus@alonsopiedra.com>
2026-04-25 20:17:49 +0200

    FINANCE - Crear categoría desde TransactionRow

--- app/finance/components/TransactionRow.tsx
+++ app/finance/components/TransactionRow.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react';
+import React, { useState } from 'react';
 import { TableRow, TableCell } from '@/components/ui/table';
 import { FinanceTransaction, FinanceCategory, FinanceAccount } from '@/types/finance';
 import { AccountAvatar } from './AccountAvatar';
